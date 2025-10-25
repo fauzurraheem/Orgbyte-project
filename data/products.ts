038b042c80e24685ad6a97c85d25e72f33f45e6d
fauzurraheem <odefunsofauzurraheem@gmail.com>
2025-10-25 19:05:57 +0100

    refactor: added a fallback to the products if it dosent fetch the data

---
+++ data/products.ts
@@ -0,0 +1,35 @@
+import { Products } from "@/types/products";
+
+
+export const fallproducts: Products = [
+  {
+    id: 1,
+    title: 'Wireless Bluetooth Headphones',
+    price: 129.99,
+    description:
+      'High-quality over-ear wireless headphones with noise cancellation and 30-hour battery life.',
+    category: 'Electronics',
+    image:
+      'https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?w=800&auto=format&fit=crop&q=80'
+  },
+  {
+    id: 2,
+    title: 'Ergonomic Office Chair',
+    price: 249.5,
+    description:
+      'Adjustable mesh office chair with lumbar support, perfect for long hours of work or gaming.',
+    category: 'Furniture',
+    image:
+      'https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?w=800&auto=format&fit=crop&q=80' 
+  },
+  {
+    id: 3,
+    title: 'Smart Fitness Watch',
+    price: 99.99,
+    description:
+      'Track your heart rate, steps, sleep, and workouts with this stylish waterproof smartwatch.',
+    category: 'Wearables',
+    image:
+      'https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?w=800&auto=format&fit=crop&q=80' 
+  }
+];
